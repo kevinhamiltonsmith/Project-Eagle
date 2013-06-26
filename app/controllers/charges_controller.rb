c97a956ffb20ef51339894feada91a2995c16c39
combizs <combiz.s@gmail.com>
2013-06-26 12:33:40 -0700

    Stripe test form works correctly

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,46 @@
 class ChargesController < ApplicationController
   def new
+    puts "*"*100
+    # # Set your secret key: remember to change this to your live secret key in production
+    # # See your keys here https://manage.stripe.com/account
+    # Stripe.api_key = 'sk_test_WJKjg9zfVpCrB5vTryRfb9Jf'
+
+    # # Get the credit card details submitted by the form
+    # token = params[:stripeToken]
+
+    # # Create the charge on Stripe's servers - this will charge the user's card
+    # begin
+    #   charge = Stripe::Charge.create(
+    #     :amount => 1000, # amount in cents, again
+    #     :currency => "usd",
+    #     :card => token,
+    #     :description => "payinguser@example.com"
+    #   )
+    # rescue Stripe::CardError => e
+    #   # The card has been declined
+    # end
   end
 
-  def create
-    # Amount in cents
-    @amount = 500
+def create
+  # Amount in cents
+  Stripe.api_key = 'sk_test_WJKjg9zfVpCrB5vTryRfb9Jf'
 
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
+  @amount = 500
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+  customer = Stripe::Customer.create(
+    :email => 'example@stripe.com',
+    :card  => params[:stripeToken]
+  )
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path
-  end
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+  )
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to charges_path
+end
 end
