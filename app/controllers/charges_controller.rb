d49be9c73cfdecc554df07bfb02b9e0d9fcad3dc
combizs <combiz.s@gmail.com>
2013-06-26 19:20:55 -0700

    added stripe form to reservation page.
    WIP: check DB to see if anyone already paid for that week and also make sure it processes payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,6 @@
 class ChargesController < ApplicationController
   def new
     puts "*"*100
-    # # Set your secret key: remember to change this to your live secret key in production
-    # # See your keys here https://manage.stripe.com/account
-    # Stripe.api_key = 'sk_test_WJKjg9zfVpCrB5vTryRfb9Jf'
-
-    # # Get the credit card details submitted by the form
-    # token = params[:stripeToken]
-
-    # # Create the charge on Stripe's servers - this will charge the user's card
-    # begin
-    #   charge = Stripe::Charge.create(
-    #     :amount => 1000, # amount in cents, again
-    #     :currency => "usd",
-    #     :card => token,
-    #     :description => "payinguser@example.com"
-    #   )
-    # rescue Stripe::CardError => e
-    #   # The card has been declined
-    # end
   end
 
   def create
