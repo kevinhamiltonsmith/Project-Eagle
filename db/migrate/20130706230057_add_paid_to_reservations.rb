e745ef3dd6c0d3433457d9b628438ae666ec612a
combizs <combiz.s@gmail.com>
2013-07-08 16:56:32 -0700

    remove extra helper files

--- db/migrate/20130706230057_add_paid_to_reservations.rb
+++
@@ -1,9 +0,0 @@
-class AddPaidToReservations < ActiveRecord::Migration
-  def self.up
-  	add_column "reservations", "paid", :string, :default => "n"
-  end
-
-  def self.down
-	remove_column "reservations", "paid"
-  end
-end
